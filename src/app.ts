31d74d01367b3fba50317e78c6901984c959fb8c
Kaifeng Yu <1536771545kai@gmail.com>
2025-01-28 00:44:25 -0500

    apply typescript

--- src/app.ts
+++ src/app.ts
@@ -3,32 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+import { Request, Response, NextFunction} from "express";  
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+const viewsPath = __dirname +"\\..\\"
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(viewsPath, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(viewsPath, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function(req:Request, res:Response, next: NextFunction) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: { message: any; status: any; }, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+import { Request, Response, NextFunction } from "express";
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
 });
 
