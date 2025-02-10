15e2e7eced8a28f3e47f707ad433b685923a6e0f
Kaifeng Yu <1536771545kai@gmail.com>
2025-02-09 23:28:08 -0500

    mount signup api

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 import { Request, Response, NextFunction } from "express";
+const{signup} = require("../controller/user");
 /* GET users listing. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
 });
 
+router.post('/signup',signup)
 module.exports = router;
